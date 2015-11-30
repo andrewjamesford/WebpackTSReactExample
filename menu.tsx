2ab7e5e76207b1b91b34a47100ba7077eb400b21
Andrew Ford <andrewfor@datacom.co.nz>
2015-11-30 13:04:31 +1300

    Further ammendmant to link stops flash

--- menu.tsx
+++ menu.tsx
@@ -18,7 +18,7 @@ export default class Menu extends React.Component<{}, {}> {
                <li><Link to="/about">About</Link></li>
             </ul>*/}
             <ul>
-               <li><a href="#/">Home</a></li>
+               <li><a href="#">Home</a></li>
                <li><a href="#/about">About</a></li>
             </ul>
          </div>
